3fcf67c3c3467de021a54e16f6f0eba30412bae2
Xuewei Niu <justxuewei@apache.org>
2022-04-22 11:58:12 +0800

    feat: stop backtrack due to segment fault
    Signed-off-by: Xuewei Niu <justxuewei@apache.org>

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-ust# project-r-core
-rCore implementation by justxuewei
+# Project rCore
+
+The project rCore implementation by justxuewei.
+
+## Contents
+
+- [os](./src/os): rCore operatering system running on riscv64.
+- [ch1-exercise1-ls](./src/ls): ls program, [reference](https://rcore-os.github.io/rCore-Tutorial-Book-v3/chapter1/7exercise.html).
+- [ch1-exercise2-backtrace](./src/backtrace): (WIP: encountered segment fault) backtrace using base pointer and frame pointer running on Linux x64, [reference](https://rcore-os.github.io/rCore-Tutorial-Book-v3/chapter1/7exercise.html)
--- src/backtrace/src/main.rs
+++ src/backtrace/src/main.rs
@@ -11,27 +11,45 @@ fn main() {
 }
 
 fn function_a() {
+    println!("=== function a ===");
     function_b();
 }
 
 fn function_b() {
+    println!("=== function b ===");
     function_c();
 }
 
 fn function_c() {
+    println!("=== function c ===");
     backtrack();
 }
 
 fn backtrack() {
     let mut base_pointer: *const usize;
+
     unsafe {
         // Refs: #2, #3
         // mov dst, src: 把 src 的数据移动到 dst 中
         // rax: 累加器
         // rbp: base pointer
-        asm!("mov {rax}, rbp", )
+        // out("reg") expr: 把 reg 数据移到 expr 中
+        asm!(
+            "mov rax, rbp", 
+            out("rax") base_pointer,
+        )
     }
 
-    let return_address = unsafe { *(base_pointer.offset(1)) } as usize;
-    println!("call site: {}", return_address);
+    // println!("base_pointer = {}", unsafe { *base_pointer } as usize);
+    // println!("base_pointer.is_null() = {}", base_pointer.is_null());
+
+    while !base_pointer.is_null() {
+        let return_address = unsafe { *(base_pointer.offset(1)) } as usize;
+        println!("call site = {}", return_address);
+        // FIXME(justxuewei): the program will exit unexpectedly after
+        // dereferenced the base_pointer.
+        base_pointer = unsafe { *base_pointer } as *const usize;
+        // println!("base_pointer = {}", unsafe { *base_pointer } as usize);
+        // println!("base_pointer.is_null() = {}", base_pointer.is_null());
+    }
 }
