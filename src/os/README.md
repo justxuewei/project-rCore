a9aa52342e8ccb7d5f567006f2580592859effc2
Xuewei Niu <justxuewei@apache.org>
2022-05-01 16:11:38 +0800

    feat: wip: implement batch os
    Signed-off-by: Xuewei Niu <justxuewei@apache.org>

--- src/os/README.md
+++ src/os/README.md
@@ -1,6 +1,6 @@
 # rCore
 
-## linker.ld
+## 编译脚本
 
 已有的内容请参阅 [调整内核的内存布局](https://rcore-os.github.io/rCore-Tutorial-Book-v3/chapter1/4first-instruction-in-kernel2.html#id4)，这里主要补充下我没有看懂的地方。
 
@@ -46,18 +46,48 @@ Ref: https://zhuanlan.zhihu.com/p/383729996
 
 执行的是对齐指令，这里不再详述。
 
-## entry.asm
+## 汇编
 
 ### .globl
 
-.globl 是创建一个全局变量，比如下面代码表示创建一个 `_start` 变量。
+.globl (.global) 是创建一个全局变量，全局变量是指对 linker 可见，这个全部变量只标志为一个名字，所以它可以是一个变量，也可以是一个数组，甚至可以是一个函数，总之这个变量与一个地址一一对应，具体内容是什么就要根据情况而定，它的地址是在链接后确定的，以 C 调用为例。
 
-```asm
-    // -- snip --
-    .globl _start
-_start:
-    // -- snip --
 ```
+/* file: a.s */
+    .section .data
+    .globl phone_num
+phone_num:
+    .word 0x1234
+    .word 0x5678
+```
+
+上面表示在代码段 (.data) 中创建一个全局变量 phone_num，它的值是 0x56781234，它的地址未知，需要等到全部链接后才能确定。
+
+```c
+// file: b.c
+#include <stdio.h>
+
+// 声明 a.s 中的全局变量 phone_num
+extern unsigned int phone_num;
+
+int main() {
+    // 打印值
+    printf("phone = 0x%x\n", phone_num);
+    // 打印指针
+    printf("&phone_num = %p\n", &phone_num);
+}
+```
+
+最终的结果是
+
+```
+phone = 0x56781234
+&phone_num = 0x601038
+```
+
+Refs:
+- https://zhuanlan.zhihu.com/p/109474273
+- https://blog.csdn.net/longintchar/article/details/80038843
 
 ### .section
 
@@ -68,4 +98,4 @@ _start:
 
 ### .space
 
-.space 表示内存布局预留空间，`.space 4096 * 16` 表示预留 64KB 空间。##
+.space 表示内存布局预留空间，`.space 4096 * 16` 表示预留 64KB 空间。
---
+++ src/os/src/batch.rs
@@ -0,0 +1,85 @@
+use core::arch::asm;
+use core::slice;
+
+use crate::sync::UPSafeCell;
+
+const MAX_APP_NUM: usize = 16;
+const APP_BASE_ADDRESS: usize = 0x80400000;
+const APP_SIZE_LIMIT: usize = 0x20000;
+
+struct AppManager {
+    num_app: usize,
+    current_app: usize,
+    app_start: [usize; MAX_APP_NUM + 1],
+}
+
+impl AppManager {
+    pub fn print_app_info(&self) {
+        println!("[kernel] num_app = {}", self.num_app);
+        for i in 0..self.num_app {
+            println!(
+                "[kernel] app_{} [{:#x},{:#x}]",
+                i,
+                self.app_start[i],
+                self.app_start[i + 1],
+            );
+        }
+    }
+
+    unsafe fn load_app(&self, app_id: usize) {
+        if app_id >= self.num_app {
+            panic!("All applications completed!");
+        }
+        println!("[kernel] Loading app_{}", app_id);
+        asm!("fence.i");
+        slice::from_raw_parts_mut(APP_BASE_ADDRESS as *mut u8, APP_SIZE_LIMIT).fill(0);
+        let app_src = slice::from_raw_parts(
+            self.app_start[app_id] as *const u8,
+            self.app_start[app_id + 1] - self.app_start[app_id],
+        );
+        let app_dst = slice::from_raw_parts_mut(APP_BASE_ADDRESS as *mut u8, app_src.len());
+        app_dst.copy_from_slice(app_src);
+    }
+
+    pub fn get_current_app(&self) -> usize {
+        self.current_app
+    }
+
+    pub fn move_to_next_app(&mut self) {
+        self.current_app += 1;
+    }
+}
+
+// 在第一次被调用的时候才真正开始加载静态变量
+lazy_static! {
+    static ref APP_MANAGER: UPSafeCell<AppManager> = unsafe {
+        UPSafeCell::new({
+            extern "C" {
+                fn _num_app();
+            }
+            let num_app_ptr = _num_app as usize as *const usize;
+            let num_app = num_app_ptr.read_volatile();
+            let mut app_start: [usize; MAX_APP_NUM + 1] = [0; MAX_APP_NUM + 1];
+            let app_start_raw: &[usize] = slice::from_raw_parts(num_app_ptr.add(1), num_app + 1);
+            app_start[..=num_app].copy_from_slice(app_start_raw);
+            AppManager {
+                num_app,
+                current_app: 0,
+                app_start,
+            }
+        })
+    };
+}
+
+pub fn init() {
+    print_app_info();
+}
+
+pub fn print_app_info() {
+    APP_MANAGER.exclusive_access().print_app_info();
+}
+
+// TODO(justxuewei): waiting for kernel implementation
+pub fn run_next_app() -> ! {
+    
+}
