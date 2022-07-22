aa407c5c3e071da98be5ee7e26bbe9062a09e88c
Xuewei Niu <justxuewei@apache.org>
2022-07-22 11:31:11 +0800

    feat(easyfs): impl block cache
    Signed-off-by: Xuewei Niu <justxuewei@apache.org>

--- src/easy-fs/src/block_dev.rs
+++ src/easy-fs/src/block_dev.rs
@@ -1,6 +1,6 @@
 use core::any::Any;
 
-// 块设备 trait
+// A trait for block device, which is implemented by OS or other libraries/applications.
 pub trait BlockDevice: Send + Sync + Any {
     fn read_block(&self, block_id: usize, buf: &mut [u8]);
     fn write_block(&self, block_id: usize, buf: &[u8]);
